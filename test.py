fa2f983d37fe918fa5a9f5a600ba6aff64a2d2ae
Samiksha Dadaji Gabhane <samiksha.gabhane@valuelabs.com>
2022-10-19 11:51:16 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,50 @@
+#import required libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+car_data = pd.read_csv(r'.\data\car_data.csv')
+
+fuel_type = car_data['Fuel_Type']
+seller_type = car_data['Seller_Type']
+transmission_type = car_data['Transmission']
+selling_price = car_data['Selling_Price']
+
+
+
+
+petrol_data = car_data.groupby('Fuel_Type').get_group('Petrol')
+petrol_data.describe()
+
+seller_data = car_data.groupby('Seller_Type').get_group('Dealer')
+seller_data.describe()
+
+#manual encoding
+car_data.replace({'Fuel_Type':{'Petrol':0, 'Diesel':1, 'CNG':2}}, inplace=True)
+#one hot encoding
+car_data = pd.get_dummies(car_data, columns=['Seller_Type', 'Transmission'], drop_first=True)
+
+
+X = car_data.drop(['Car_Name','Selling_Price'], axis=1)
+y = car_data['Selling_Price']
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+
+print(pred)
